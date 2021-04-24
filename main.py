0ba4dde94e9b3792f6c8a90b7531b2b7d4299e74
kdvalishivli <kdvalishvili@opus2.com>
2021-04-25 00:14:37 +0400

    Derefactor

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from mail_sender import send_main
+import ssl
+import smtplib
+
+def send_main():
+    smtp_server = "smtp.gmail.com"
+    port = 587
+    sender_email = "almightybuttoninfo@gmail.com"
+    sender_password = "ASDasd123."
+    receiver_email = "cdval13@freeuni.edu.ge"
+    message = 'Gasswore davalebebi daidos qulebi'
+    context = ssl.create_default_context()
+    try:
+        server = smtplib.SMTP(smtp_server, port)
+        server.ehlo()
+        server.starttls(context=context)
+        server.ehlo()
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, receiver_email, message)
+        return {"Message": "Mail has been sent."}
+    except Exception as e:
+        print(e)
+        return {"Message": "Error occurred while sending mail."}
+    finally:
+        server.quit()
 
 app = FastAPI()
 
